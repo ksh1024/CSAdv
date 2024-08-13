b9b7d895fdc5dd383d1bdff4598914b6801973cb
kim <ksh251024@gmail.com>
2024-08-13 09:51:14 +0900

    제네릭 실습1

--- CSAdv/Program.cs
+++ CSAdv/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CSAdv
 {
+    class Wanted<T>
+    {
+        public T Value;
+        public Wanted(T value)
+        {
+            Value = value;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Wanted<int> wantedInt = new Wanted<int>(30302);
+            Wanted<string> wantedString = new Wanted<string>("hello world");
+            Wanted<double> wantedDouble = new Wanted<double>(33.3);
+
+            Console.WriteLine(wantedInt.Value);
+            Console.WriteLine(wantedString.Value);
+            Console.WriteLine(wantedDouble.Value);
         }
     }
 }
